ef59c33eaaec3d4cb46f747e14f0a8c560dc8aec
Hassan Hammoud <hassanhammoud2000@gmail.com>
2025-09-30 08:12:40 -0700

    finished feed page ui

---
+++ src/Components/Feed/Feed.jsx
@@ -0,0 +1,219 @@
+/** @format */
+
+import React from 'react';
+import './Feed.css';
+import { assets } from '../../assets/assets';
+const Feed = () => {
+  return (
+    <div className='feed'>
+      {/*  Start Feed 1  */}
+      <div className='card'>
+        <img
+          src={assets.thumbnail1}
+          alt='thumbnail1'
+        />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>HassanTech</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+
+      {/*  Start Feed 2  */}
+      <div className='card'>
+        <img
+          src={assets.thumbnail2}
+          alt='thumbnail2'
+        />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>HassanTech</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+
+      {/*  Start Feed 3  */}
+      <div className='card'>
+        <img
+          src={assets.thumbnail3}
+          alt='thumbnail3'
+        />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>HassanTech</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+
+      {/*  Start Feed 4  */}
+      <div className='card'>
+        <img
+          src={assets.thumbnail4}
+          alt='thumbnail4'
+        />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>HassanTech</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+
+      {/*  Start Feed 5  */}
+      <div className='card'>
+        <img
+          src={assets.thumbnail5}
+          alt='thumbnail5'
+        />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>HassanTech</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+
+      {/*  Start Feed 6  */}
+      <div className='card'>
+        <img
+          src={assets.thumbnail6}
+          alt='thumbnail6'
+        />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>HassanTech</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+
+      {/*  Start Feed 7  */}
+      <div className='card'>
+        <img
+          src={assets.thumbnail7}
+          alt='thumbnail7'
+        />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>HassanTech</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+
+      {/*  Start Feed 8  */}
+      <div className='card'>
+        <img
+          src={assets.thumbnail8}
+          alt='thumbnail8'
+        />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>HassanTech</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+      {/*  Start Feed 1  */}
+      <div className='card'>
+        <img
+          src={assets.thumbnail1}
+          alt='thumbnail1'
+        />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>HassanTech</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+
+      {/*  Start Feed 2  */}
+      <div className='card'>
+        <img
+          src={assets.thumbnail2}
+          alt='thumbnail2'
+        />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>HassanTech</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+
+      {/*  Start Feed 3  */}
+      <div className='card'>
+        <img
+          src={assets.thumbnail3}
+          alt='thumbnail3'
+        />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>HassanTech</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+
+      {/*  Start Feed 4  */}
+      <div className='card'>
+        <img
+          src={assets.thumbnail4}
+          alt='thumbnail4'
+        />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>HassanTech</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+
+      {/*  Start Feed 5  */}
+      <div className='card'>
+        <img
+          src={assets.thumbnail5}
+          alt='thumbnail5'
+        />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>HassanTech</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+
+      {/*  Start Feed 6  */}
+      <div className='card'>
+        <img
+          src={assets.thumbnail6}
+          alt='thumbnail6'
+        />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>HassanTech</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+
+      {/*  Start Feed 7  */}
+      <div className='card'>
+        <img
+          src={assets.thumbnail7}
+          alt='thumbnail7'
+        />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>HassanTech</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+
+      {/*  Start Feed 8  */}
+      <div className='card'>
+        <img
+          src={assets.thumbnail8}
+          alt='thumbnail8'
+        />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>HassanTech</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+    </div>
+  );
+};
+
+export default Feed;
--- src/assets/assets.js
+++ src/assets/assets.js
@@ -21,6 +21,14 @@ import simon from './simon.png';
 import tom from './tom.png';
 import megan from './megan.png';
 import cameron from './cameron.png';
+import thumbnail1 from './thumbnail1.png';
+import thumbnail2 from './thumbnail2.png';
+import thumbnail3 from './thumbnail3.png';
+import thumbnail4 from './thumbnail4.png';
+import thumbnail5 from './thumbnail5.png';
+import thumbnail6 from './thumbnail6.png';
+import thumbnail7 from './thumbnail7.png';
+import thumbnail8 from './thumbnail8.png';
 
 export const assets = {
   menu_icon,
@@ -43,4 +51,12 @@ export const assets = {
   tom,
   megan,
   cameron,
+  thumbnail1,
+  thumbnail2,
+  thumbnail3,
+  thumbnail4,
+  thumbnail5,
+  thumbnail6,
+  thumbnail7,
+  thumbnail8,
 };
