c36a1badd67a8ddb20fec452c272835f18a497e4
Hassan Hammoud <hassanhammoud2000@gmail.com>
2025-10-01 10:36:31 -0700

    Finished Recommended video side

---
+++ src/Components/Recommended/Recommended.jsx
@@ -0,0 +1,108 @@
+/** @format */
+
+import { assets } from '../../assets/assets';
+import './Recommended.css';
+const Recommended = () => {
+  return (
+    <div className='recommended'>
+      {/* Side List Video-1  */}
+      <div className='side-video-list'>
+        <img
+          src={assets.thumbnail1}
+          alt='Thumbnail1'
+        />
+        <div className='vid-info'>
+          <h4>Best Channel That Help You To Be A Web Developer</h4>
+          <p>HassanTech</p>
+          <p>199K Views</p>
+        </div>
+      </div>
+      {/* Side List Video-2  */}
+      <div className='side-video-list'>
+        <img
+          src={assets.thumbnail2}
+          alt='Thumbnail2'
+        />
+        <div className='vid-info'>
+          <h4>Best Channel That Help You To Be A Web Developer</h4>
+          <p>HassanTech</p>
+          <p>199K Views</p>
+        </div>
+      </div>
+      {/* Side List Video-3  */}
+      <div className='side-video-list'>
+        <img
+          src={assets.thumbnail3}
+          alt='Thumbnail3'
+        />
+        <div className='vid-info'>
+          <h4>Best Channel That Help You To Be A Web Developer</h4>
+          <p>HassanTech</p>
+          <p>199K Views</p>
+        </div>
+      </div>
+      {/* Side List Video-4  */}
+      <div className='side-video-list'>
+        <img
+          src={assets.thumbnail4}
+          alt='Thumbnail4'
+        />
+        <div className='vid-info'>
+          <h4>Best Channel That Help You To Be A Web Developer</h4>
+          <p>HassanTech</p>
+          <p>199K Views</p>
+        </div>
+      </div>
+      {/* Side List Video-5  */}
+      <div className='side-video-list'>
+        <img
+          src={assets.thumbnail5}
+          alt='Thumbnail5'
+        />
+        <div className='vid-info'>
+          <h4>Best Channel That Help You To Be A Web Developer</h4>
+          <p>HassanTech</p>
+          <p>199K Views</p>
+        </div>
+      </div>
+      {/* Side List Video-6  */}
+      <div className='side-video-list'>
+        <img
+          src={assets.thumbnail6}
+          alt='Thumbnail6'
+        />
+        <div className='vid-info'>
+          <h4>Best Channel That Help You To Be A Web Developer</h4>
+          <p>HassanTech</p>
+          <p>199K Views</p>
+        </div>
+      </div>
+      {/* Side List Video-7  */}
+      <div className='side-video-list'>
+        <img
+          src={assets.thumbnail7}
+          alt='Thumbnail7'
+        />
+        <div className='vid-info'>
+          <h4>Best Channel That Help You To Be A Web Developer</h4>
+          <p>HassanTech</p>
+          <p>199K Views</p>
+        </div>
+      </div>
+      {/* Side List Video-8  */}
+      <div className='side-video-list'>
+        <img
+          src={assets.thumbnail8}
+          alt='Thumbnail8'
+        />
+        <div className='vid-info'>
+          <h4>Best Channel That Help You To Be A Web Developer</h4>
+          <p>HassanTech</p>
+          <p>199K Views</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Recommended;
--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -1,12 +1,14 @@
 /** @format */
 
 import PlayVideo from '../../Components/PlayVideo/PlayVideo';
+import Recommended from '../../Components/Recommended/Recommended';
 import './Video.css';
 
 const Video = () => {
   return (
     <div className='play-container'>
       <PlayVideo />
+      <Recommended />
     </div>
   );
 };
