48501032f733ae12dd83d34c4df37ca9ef0a6902
Hassan Hammoud <hassanhammoud2000@gmail.com>
2025-10-01 04:09:25 -0700

    finished play video UI

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -1,8 +1,323 @@
 /** @format */
+import { assets } from '../../assets/assets';
 import './PlayVideo.css';
 
 const PlayVideo = () => {
-  return <div></div>;
+  return (
+    <div className='play-video'>
+      <video
+        src={assets.video1}
+        controls
+        autoPlay
+        muted
+      ></video>
+      <h3>Best YouTube Channel to Learn Web Development</h3>
+      <div className='play-video-info'>
+        <p>1525 Views &bull; 2 days ago</p>
+        <div className=''>
+          <span>
+            <img
+              src={assets.like}
+              alt='Like Image'
+            />
+            125
+          </span>
+
+          <span>
+            <img
+              src={assets.dislike}
+              alt='DisLike Image'
+            />
+            2
+          </span>
+          <span>
+            <img
+              src={assets.share}
+              alt='Share Image'
+            />
+            Share
+          </span>
+          <span>
+            <img
+              src={assets.save}
+              alt='Save Image'
+            />
+            Save
+          </span>
+        </div>
+      </div>
+      <hr />
+      <div className='publisher'>
+        <img
+          src={assets.jack}
+          alt=''
+        />
+        <div className=''>
+          <p>HassanTech</p>
+          <span>1M Subscribers</span>
+        </div>
+        <button>Subscribe</button>
+      </div>
+      <div className='vid-description'>
+        <p>Channel that makes learning easy</p>
+        <p>Subscribe HassanTech To Watch More Tutorials On Web Development</p>
+        <hr />
+        <h4>130 Comments</h4>
+        {/*  */}
+        <div className='comment'>
+          <img
+            src={assets.user_profile}
+            alt='User Profile Image'
+          />
+          <div className=''>
+            <h3>
+              Jack Nicholson <span>1 day ago</span>
+            </h3>
+            <p>
+              A global computer network providing a variety of information and
+              cc of interconnected networks using standardized communication
+              protocols.
+              <div className='comment-action'>
+                <img
+                  src={assets.like}
+                  alt=''
+                />
+                <span>244</span>
+                <img
+                  src={assets.dislike}
+                  alt='DisLike Image'
+                />
+              </div>
+            </p>
+          </div>
+        </div>
+        {/*  */}
+        <div className='comment'>
+          <img
+            src={assets.user_profile}
+            alt='User Profile Image'
+          />
+          <div className=''>
+            <h3>
+              Jack Nicholson <span>1 day ago</span>
+            </h3>
+            <p>
+              A global computer network providing a variety of information and
+              cc of interconnected networks using standardized communication
+              protocols.
+              <div className='comment-action'>
+                <img
+                  src={assets.like}
+                  alt=''
+                />
+                <span>244</span>
+                <img
+                  src={assets.dislike}
+                  alt='DisLike Image'
+                />
+              </div>
+            </p>
+          </div>
+        </div>
+        {/*  */}
+        <div className='comment'>
+          <img
+            src={assets.user_profile}
+            alt='User Profile Image'
+          />
+          <div className=''>
+            <h3>
+              Jack Nicholson <span>1 day ago</span>
+            </h3>
+            <p>
+              A global computer network providing a variety of information and
+              cc of interconnected networks using standardized communication
+              protocols.
+              <div className='comment-action'>
+                <img
+                  src={assets.like}
+                  alt=''
+                />
+                <span>244</span>
+                <img
+                  src={assets.dislike}
+                  alt='DisLike Image'
+                />
+              </div>
+            </p>
+          </div>
+        </div>
+        {/*  */}
+        <div className='comment'>
+          <img
+            src={assets.user_profile}
+            alt='User Profile Image'
+          />
+          <div className=''>
+            <h3>
+              Jack Nicholson <span>1 day ago</span>
+            </h3>
+            <p>
+              A global computer network providing a variety of information and
+              cc of interconnected networks using standardized communication
+              protocols.
+              <div className='comment-action'>
+                <img
+                  src={assets.like}
+                  alt=''
+                />
+                <span>244</span>
+                <img
+                  src={assets.dislike}
+                  alt='DisLike Image'
+                />
+              </div>
+            </p>
+          </div>
+        </div>
+        {/*  */}
+        <div className='comment'>
+          <img
+            src={assets.user_profile}
+            alt='User Profile Image'
+          />
+          <div className=''>
+            <h3>
+              Jack Nicholson <span>1 day ago</span>
+            </h3>
+            <p>
+              A global computer network providing a variety of information and
+              cc of interconnected networks using standardized communication
+              protocols.
+              <div className='comment-action'>
+                <img
+                  src={assets.like}
+                  alt=''
+                />
+                <span>244</span>
+                <img
+                  src={assets.dislike}
+                  alt='DisLike Image'
+                />
+              </div>
+            </p>
+          </div>
+        </div>
+        {/*  */}
+        <div className='comment'>
+          <img
+            src={assets.user_profile}
+            alt='User Profile Image'
+          />
+          <div className=''>
+            <h3>
+              Jack Nicholson <span>1 day ago</span>
+            </h3>
+            <p>
+              A global computer network providing a variety of information and
+              cc of interconnected networks using standardized communication
+              protocols.
+              <div className='comment-action'>
+                <img
+                  src={assets.like}
+                  alt=''
+                />
+                <span>244</span>
+                <img
+                  src={assets.dislike}
+                  alt='DisLike Image'
+                />
+              </div>
+            </p>
+          </div>
+        </div>
+        {/*  */}
+        <div className='comment'>
+          <img
+            src={assets.user_profile}
+            alt='User Profile Image'
+          />
+          <div className=''>
+            <h3>
+              Jack Nicholson <span>1 day ago</span>
+            </h3>
+            <p>
+              A global computer network providing a variety of information and
+              cc of interconnected networks using standardized communication
+              protocols.
+              <div className='comment-action'>
+                <img
+                  src={assets.like}
+                  alt=''
+                />
+                <span>244</span>
+                <img
+                  src={assets.dislike}
+                  alt='DisLike Image'
+                />
+              </div>
+            </p>
+          </div>
+        </div>
+        {/*  */}
+        <div className='comment'>
+          <img
+            src={assets.user_profile}
+            alt='User Profile Image'
+          />
+          <div className=''>
+            <h3>
+              Jack Nicholson <span>1 day ago</span>
+            </h3>
+            <p>
+              A global computer network providing a variety of information and
+              cc of interconnected networks using standardized communication
+              protocols.
+              <div className='comment-action'>
+                <img
+                  src={assets.like}
+                  alt=''
+                />
+                <span>244</span>
+                <img
+                  src={assets.dislike}
+                  alt='DisLike Image'
+                />
+              </div>
+            </p>
+          </div>
+        </div>
+        {/*  */}
+        <div className='comment'>
+          <img
+            src={assets.user_profile}
+            alt='User Profile Image'
+          />
+          <div className=''>
+            <h3>
+              Jack Nicholson <span>1 day ago</span>
+            </h3>
+            <p>
+              A global computer network providing a variety of information and
+              cc of interconnected networks using standardized communication
+              protocols.
+              <div className='comment-action'>
+                <img
+                  src={assets.like}
+                  alt=''
+                />
+                <span>244</span>
+                <img
+                  src={assets.dislike}
+                  alt='DisLike Image'
+                />
+              </div>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PlayVideo;
--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,26 +1,30 @@
 /** @format */
 
-import menu_icon from './menu.png';
-import logo from './logo.png';
-import search_icon from './search.png';
-import upload_icon from './upload.png';
-import more_icon from './more.png';
-import notification_icon from './notification.png';
-import profile_icon from './jack.png';
-import home from './home.png';
-import game_icon from './game_icon.png'; // ?
 import automobiles from './automobiles.png';
-import sports from './sports.png';
-import entertainment from './entertainment.png';
-import tech from './tech.png';
-import music from './music.png';
 import blogs from './blogs.png';
+import entertainment from './entertainment.png';
+import game_icon from './game_icon.png'; // ?
+import home from './home.png';
+import profile_icon from './jack.png';
+import logo from './logo.png';
+import menu_icon from './menu.png';
+import more_icon from './more.png';
+import music from './music.png';
 import news from './news.png';
+import notification_icon from './notification.png';
+import search_icon from './search.png';
+import sports from './sports.png';
+import tech from './tech.png';
+import upload_icon from './upload.png';
 // import jak from './home.png';
-import simon from './simon.png';
-import tom from './tom.png';
-import megan from './megan.png';
 import cameron from './cameron.png';
+import dislike from './dislike.png';
+import jack from './jack.png';
+import like from './like.png';
+import megan from './megan.png';
+import save from './save.png';
+import share from './share.png';
+import simon from './simon.png';
 import thumbnail1 from './thumbnail1.png';
 import thumbnail2 from './thumbnail2.png';
 import thumbnail3 from './thumbnail3.png';
@@ -29,6 +33,9 @@ import thumbnail5 from './thumbnail5.png';
 import thumbnail6 from './thumbnail6.png';
 import thumbnail7 from './thumbnail7.png';
 import thumbnail8 from './thumbnail8.png';
+import tom from './tom.png';
+import user_profile from './user_profile.jpg';
+import video1 from './video.mp4';
 
 export const assets = {
   menu_icon,
@@ -59,4 +66,11 @@ export const assets = {
   thumbnail6,
   thumbnail7,
   thumbnail8,
+  save,
+  jack,
+  user_profile,
+  video1,
+  like,
+  share,
+  dislike,
 };
